2212096a34d28e073af56f7b3c06e92962257d8c
peterzm <pzabalamedina@gmail.com>
2018-09-03 19:23:55 -0300

    Script de prueba para generar sonido usando la libería pyaudio

---
+++ audio.py
@@ -0,0 +1,28 @@
+import pyaudio
+import sounddevice
+import numpy as np
+import matplotlib as plt
+
+p = pyaudio.PyAudio()
+
+volume = 1.0     # range [0.0, 1.0]
+fs = 44800       # sampling rate, Hz, must be integer
+duration = 60.0   # in seconds, may be float
+f = 440.0        # sine frequency, Hz, may be float
+
+# generate samples, note conversion to float32 array
+samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+
+# for paFloat32 sample values must be in range [-1.0, 1.0]
+stream = p.open(format=pyaudio.paFloat32,
+                channels=1,
+                rate=fs,
+                output=True)
+
+# play. May repeat with different volume values (if done interactively) 
+stream.write(volume*samples)
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
