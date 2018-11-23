c9c765a4b39ebecd002c851d07878734f214e8cd
peterzm <pzabalamedina@gmail.com>
2018-11-23 11:33:58 -0300

    resultados lazo de control para kp

--- Daq/controla_temperatura_on_off_contador.py
+++ Daq/controla_temperatura_on_off_contador.py
@@ -28,9 +28,10 @@ print(list(s.devices)) # data dev correspondiente
 #%% 
 fs = 1
 samples = 1
-setpoint =40
-kp=0.05
-periodoPWM = 0.1
+setpoint =50
+kp=0.1
+ki=0.2
+periodoPWM = 4.0
 plt.ion()   #habilita modo interactivo de matplotlib
 
 listTemp = []
@@ -51,16 +52,39 @@ def ferror(Temp2,setpoint,kp):
     elif c<-0.99:
             c=-0.99
     return c
-        
+'''        
+def PI(Temp2,setpoint,kp,ki):
+    errorTemp = setpoint - Temp2
+    c = kp*errorTemp
+    i_term += error
+    #pi = c + 
+    return c + (ki * i_term), i_term
+'''    
+    
+def PIDFun(feedback_value, setpoint, kp, ki, kd, last_error, i_term):
+    error = setpoint - feedback_value
 
+    delta_error = error - last_error
+
+    p_term = kp * error
+    i_term += error
+    d_term = delta_error
+
+    last_error = delta_error
+
+    return p_term + (ki * i_term) + (kd * d_term), last_error, i_term    
+    
+    
+    
 def contador():
 
      i=0
      with daq.Task() as task:
-         task.co_channels.add_co_pulse_chan_time("Dev3/ctr0",name_to_assign_to_channel = "pwmOut",
+         task.co_channels.add_co_pulse_chan_time("Dev13/ctr0",name_to_assign_to_channel = "pwmOut",
                                                  high_time=periodoPWM/2, low_time=periodoPWM/2)
          task.timing.cfg_implicit_timing(sample_mode=daq.constants.AcquisitionType.CONTINUOUS, samps_per_chan=700)
          task.start()
+         
          temperatura=medir_tem()
          while(True):
              temperatura=0.8*temperatura+0.2*medir_tem()
@@ -72,7 +96,7 @@ def contador():
             #plt.scatter(i,data[1],c='b')
             
              print("Temperatura:", temperatura)
-             np.savetxt('Temperatura_prue_final.txt', listTemp, delimiter=' ')
+             np.savetxt('Temperatura_prue_final__kp01_1.txt', listTemp, delimiter=' ')
                                      
              i +=1
                           
@@ -83,9 +107,9 @@ def contador():
              C_.append(c)
              ten_.append(tiempo_encendido)
              tap_.append(tiempo_apagado)
-             np.savetxt('C_1.txt', C_, delimiter=' ')
-             np.savetxt('ten_1.txt', ten_, delimiter=' ')
-             np.savetxt('tap_1.txt', tap_, delimiter=' ')
+             np.savetxt('C_1_kp01_1.txt', C_, delimiter=' ')
+             np.savetxt('ten_1_kp_01_1.txt', ten_, delimiter=' ')
+             np.savetxt('tap_1_kp_01_1.txt', tap_, delimiter=' ')
              
              plt.subplot(212)
              plt.scatter(i,temperatura,c='r', label= 'temperatura')
@@ -108,23 +132,27 @@ def contador():
 
 
 
-def activa_salida_digital(activa_o_desactiva):
+def activa_salida_digital():
     #nidaqmx._task_modules.channels.channel.Channel 
     #nidaqmx.system._collections.PhysicalChannelCollection
     with daq.Task() as task:
         #daq.constants.ChannelType(10153)
         task.do_channels.add_do_chan(
-            "Dev3/port0/line0", line_grouping=LineGrouping.CHAN_PER_LINE)
-        #samples = [True] 
-        task.write(activa_o_desactiva)    
+            "Dev13/port0/line0", line_grouping=LineGrouping.CHAN_PER_LINE)
+        #samples = [True]
+        while (True):
+            time.sleep(periodoPWM)
+            task.write(False)
+            time.sleep(periodoPWM)
+            task.write(True)
 
 
 def medir_tem():
 
     with daq.Task() as task:
-        ai0 = task.ai_channels.add_ai_voltage_chan("Dev3/ai0",terminal_config = daq.constants.TerminalConfiguration(10083))
+        ai0 = task.ai_channels.add_ai_voltage_chan("Dev13/ai0",terminal_config = daq.constants.TerminalConfiguration(10083))
         ai0.ai_gain=1
-        ai1 = task.ai_channels.add_ai_voltage_chan("Dev3/ai1",terminal_config = daq.constants.TerminalConfiguration(10083))
+        ai1 = task.ai_channels.add_ai_voltage_chan("Dev13/ai1",terminal_config = daq.constants.TerminalConfiguration(10083))
         ai1.ai_gain=1
         #task.start()
         task.timing.cfg_samp_clk_timing(fs) # seteo la frecuencia de muestreo
@@ -138,24 +166,6 @@ def medir_tem():
         Temp2 = Temp * 100
 
     return Temp2
-'''        
-        c = ferror(Temp2,setpoint,kp)
-        if c>0.99:
-            c=0.99
-        elif c<-0.99:
-            c=-0.99
-        
-        tiempo_encendido=tiempo_total*(1-c)
-        activa = activa_salida_digital(activa_o_desactiva=False)
-        time.sleep(tiempo_encendido)
-        tiempo_apagado=tiempo_total*(1+c)
-        desactiva = activa_salida_digital(activa_o_desactiva=True)
-        time.sleep(tiempo_apagado)
-        #print(data)
-        #if Temp2<50:
-            #activa = activa_salida_digital(activa_o_desactiva=False)
-        #if Temp2>55:
-            #desactiva = activa_salida_digital(activa_o_desactiva=True)
-'''
+
 
 
